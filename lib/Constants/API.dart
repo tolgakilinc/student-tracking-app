fd05950fbc85e4ad7b9e29fb5b8c1f09917ed57d
tolgakilinc <furkan33arda@gmail.com>
2023-01-21 16:55:44 +0300

    git resimleri eklendi

--- lib/Constants/API.dart
+++ lib/Constants/API.dart
@@ -1,4 +1,4 @@
-const String address = "192.168.110.208";
+const String address = "192.168.1.106";
 //const String address = "10.10.58.125";
 //const String address = "10.10.13.159";192.168.112.208
 /*const String port = "60953";*/
